9372dd3bf6c84cf8324a4322656cb565ffefa1dd
qq112139-hub <qq112139@gmail.com>
2026-04-16 15:34:39 +0900

    Add files via upload

---
+++ Portfolio/ai_model/ai_interface.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+from ai_model.conv_ai import readImage
+def model_pred(file_path):
+    #file_path: 업로드된 이미지 전체 경로
+    result = readImage(file_path)
+    return result
---
+++ Portfolio/static/app/index.js
@@ -0,0 +1,33 @@
+//index.js
+console.log("javascript -- running")
+$(()=>{//ready 이벤트 작동
+    //console.log($("h1"))
+
+    $("li").on("mouseover",function(){
+        const img_path="/static/images/"
+        const img_style=`style="width:50vw;max-height:67vh"`
+        const img_tag = `<img ${img_style} src="${img_path}${$(this).attr("im")}">`
+        $("#main_img").html(img_tag)
+    })
+    $("li").on("click",function(){
+        $("#send_service").attr("info",this.id)
+        $("#content_container").css("display","block")
+    })
+    $("#send_service").on("click",async function(){
+        const form_data = new FormData();
+        form_data.append("user_img",$("#file_input").get(0).files[0])
+        const pending = await fetch(`/option/${$(this).attr("info")}`,{
+            method:"post",
+            body:form_data
+        })
+        const result = await pending.json()
+        console.log(result)
+        $("#result").append(`
+        <div style = "text-align:center;width:12rem;float:left">
+        <img style = "width:11em;height:11empx" src = "data:image/png;base64,${result.timg}"
+        <p id = "predtext" style="margin:1rem">${result.yart} ${result.y_pred}로 예측</p>
+        </div>
+        `)
+    })
+    $("#close").on("click",()=>{$("#content_container").css("display","none")})
+})
